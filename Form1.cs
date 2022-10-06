6c91614882020498d2017f8afee81fea46ab2f68
SparkySimp <ozturkyigitcemal@gmail.com>
2022-10-06 09:04:26 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTP_120622_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_click(object sender, EventArgs e)
+        { 
+            var btn = (Button)sender;
+            if(btn.BackColor != Color.Red )
+                btn.BackColor = Color.Red;
+        }
+    }
+}
